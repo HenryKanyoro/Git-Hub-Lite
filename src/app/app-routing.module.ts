718090ed9aa58dfbcd658fa898d37935b418ab3e
HenryKanyoro <kanyorohenry6@gmail.com>
2020-11-16 16:29:22 +0300

    linked the components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import { SearchComponent} from './search/search.component'
+import { RepositoriesComponent} from './repositories/repositories.component';
+import { from } from 'rxjs';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+    {path: 'search', component: SearchComponent},
+    {path: 'repository', component: RepositoriesComponent},
+    {path: '', redirectTo: '/search', pathMatch: 'full'},
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class RoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { RoutingModule} from './app-routing.module'
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { SearchComponent } from './search/search.component';
 
+
+import { FormsModule } from '@angular/forms';
+import { RepositoriesComponent } from './repositories/repositories.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { SearchComponent } from './search/search.component';
+import {SearchRequestService} from '../app/search-request-service.service';
+import { NavbarComponent } from './navbar/navbar.component';
+import { from } from 'rxjs';
+import { OnHoverDirective } from './on-hover.directive';
+import { DateCountPipe } from './date-count.pipe';
 @NgModule({
   declarations: [
     AppComponent,
+    RepositoriesComponent,
+    SearchFormComponent,
+    SearchComponent,
     NavbarComponent,
-    SearchComponent
+    OnHoverDirective,
+    DateCountPipe
+  
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [SearchRequestService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class AppBootstrapModule { }
+
